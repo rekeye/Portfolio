a09198242b8f7e516dd35f0eb55f42b864d777a6
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-07-15 15:57:35 +0200

    further development of the layout, changing scss architecture

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "none",
   module: {
     rules: [
       {
@@ -10,51 +11,50 @@ module.exports = {
         use: [
           {
             loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.(png|svg|jpg|gf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
+            options: {
+              publicPath: "images",
+            },
           },
         ],
-      }
-    ]
+      },
+    ],
   },
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
+      template: "./src/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-  ]
+  ],
 };
