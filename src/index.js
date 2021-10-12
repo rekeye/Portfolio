460d561ceb2b718fd971b0df17fcd289c6338dcb
Szymon <12szymon.paluch34@gmail.com>
2021-10-12 21:22:10 +0200

    Revert "removing dist from master"
    This reverts commit 351f7516e07b293a1c02dd37a8e7a9f9b19f068b.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//styles
-import "./scss/main.scss";
-//event listeners
-import "./js/event/scroll";
-//content
-import "./js/content/work";
-import "./js/content/stickyNav";
-//api
-import "./js/api/githubRepos";
