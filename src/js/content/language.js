a2691706abcc2441850892d7e140d8c588698428
Szymon <12szymon.paluch34@gmail.com>
2021-10-19 00:46:28 +0200

    implemented navigator.language property for elimination of unwanted prompt to allow geolocation

--- src/js/content/language.js
+++ src/js/content/language.js
@@ -1,18 +1,16 @@
 // this file handles the switching of languages
 // function in this file is triggered by event listener in /src/js/event/languageButton.js
 
-import locationAPI from "../api/location";
+//import chooseLang helper for cleaner code
 import chooseLang from "../helpers/chooseLang";
-import error from "../helpers/error";
 
 // constant variables for error reduction
 const PL = "PL",
   EN = "EN";
 
 const language = (storage) => {
-  // dom element grabbers
   const button = document.getElementById("switch-language"),
-    lang = storage.get("lang");
+    cookieLang = storage.get("lang");
   const buttons = {
     en: document.getElementById("en"),
     pl: document.getElementById("pl"),
@@ -22,49 +20,25 @@ const language = (storage) => {
     pl: document.querySelectorAll('[lang="pl"]'),
   };
 
-  // Initially disable language switching button. This will be enabled by chooseLang function after everything is set up
+  // initially disable language switching button this will be enabled by chooseLang function after everything is set up
   button.setAttribute("disabled", "disabled");
 
-  // Check if language cookie already exists.
-  if (lang) {
-    if (lang === PL) {
-      chooseLang(button, buttons.pl, elements.en, elements.pl);
+  if (cookieLang) {
+    if (cookieLang === PL) {
+      chooseLang(button, buttons.pl, elements.pl, elements.en);
     } else {
-      chooseLang(button, buttons.en, elements.pl, elements.en);
+      chooseLang(button, buttons.en, elements.en, elements.pl);
     }
   } else {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async ({ coords: { lat, lng } }) => {
-          //make the api call for users country
-          const country = await locationAPI(lat, lng);
-
-          if (country === PL) {
-            chooseLang(button, buttons.pl, elements.en, elements.pl, {
-              storage,
-              langCookie: PL,
-            });
-          } else {
-            chooseLang(button, buttons.en, elements.pl, elements.en, {
-              storage,
-              langCookie: EN,
-            });
-          }
-        },
-        (err) => {
-          if (err.code === err.PERMISSION_DENIED) {
-            error("Permission denied. Defaulting to English!");
-          } else {
-            error("Unknown error. Defaulting to English!");
-          }
-          chooseLang(button, buttons.en, elements.pl, elements.en, {
-            storage,
-            langCookie: EN,
-          });
-        }
-      );
+    // check users language using the navigator.language property, docs: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/language
+    if (/^pl\b/.test(navigator.language)) {
+      console.log(navigator.language);
+      chooseLang(button, buttons.pl, elements.pl, elements.en, {
+        storage,
+        langCookie: PL,
+      });
     } else {
-      chooseLang(button, buttons.en, elements.pl, elements.en, {
+      chooseLang(button, buttons.en, elements.en, elements.pl, {
         storage,
         langCookie: EN,
       });
